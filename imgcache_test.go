e27a5d019182e0c0c1bfa4beba244e9b5a72e9de
Evan Lin <evanslin@gmail.com>
2021-03-25 15:54:24 +0800

    fix test case

--- imgcache_test.go
+++ imgcache_test.go
@@ -11,9 +11,7 @@ func init() {
 func TestGetImgCacheJPG(t *testing.T) {
 
 	var testingUrls = []string{
-		"http://asms.coa.gov.tw/amlapp/upload/pic/c74571c1-0088-4d9f-a853-1177516d627b_org.jpg",
-		"http://asms.coa.gov.tw/amlapp/upload/pic/acd5caf0-1c33-43e5-a97e-b670e15adc6f_org.JPG",
-		"http://asms.coa.gov.tw/amlapp/upload/pic/44632381-d1a0-4aa9-af2e-334f157f9847_org.JPG",
+		"https://www.coa.gov.tw/files/masthead/24/A01_1.jpg",
 	}
 
 	for _, v := range testingUrls {
